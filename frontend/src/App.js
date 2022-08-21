f02d3376030f4e58c3229425c226d7af0b7c4342
Uttaran Das <uttarandas501@gmail.com>
2022-08-21 21:39:16 +0530

    Initial Code

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Home from "./components/Home";
+
+function App() {
+  return (
+    <div>
+    <NavBar/>
+    </div>
+  );
+}
+
+export default App;
